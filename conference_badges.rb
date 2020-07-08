ff2679fdc80e2fdca003ab4c9f91c6a3cff63808
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-08 19:13:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  arr2=Array.new
+  array.each do |name|
+    arr2.push(badge_maker(name))
+  end
+  return arr2
+end
+
+def assign_rooms(array)
+  arr2=Array.new
+  number=1
+  array.each do |name|
+    ch="Hello, #{name}! You'll be assign to room #{number}"
+    arr2.push(ch)
+    number+=1
+  end
+end
+
+
+    
